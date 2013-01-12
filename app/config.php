d50169f769fb85403508016b0872f3ea9fa5dee8
Sokifilou <07fortune@gmail.com>
2013-01-12 18:05:36 +0100

    Update app/config.php
    some info in config

--- app/config.php
+++ app/config.php
@@ -14,14 +14,14 @@ $AppConfig = array (
 	),
 	'system' 	=> array (
 		'lang'				=> 'en',										// this is the default language, ar = for arabic, en = for english
-		'forum_url'			=> 'x',                                        // ton fofo
+		'forum_url'			=> 'x',                                        //you forum
 		'social_url'		=> 'x',
 		// admin account info
 		'adminName'			=> 'admin',
 		'adminPassword'		=> '123',
 		'admin_email'		=> 'admin@admin.com',			// the email for admin account (set it before setup)
 		'email' 			=> 'admin@admin.com',			// the email for others (like activation, forget password, ..etc)
-		'installkey' 			=> 'installer',
+		'installkey' 			=> 'install',
 		'calltatar' 			=> 'tatar'
 	),
 	'plus'			=> array (
@@ -30,28 +30,28 @@ $AppConfig = array (
 				'name'		=> 'Package A',
 				'gold'		=> 30,
 				'cost'		=> 1.49,
-				'currency'	=> 'usd',
+				'currency'	=> 'usd', //type USD to show in dollar and EUR to make in euro
 				'image'		=> 'package_a.jpg'
 			),
 			array ( 
 				'name'		=> 'Package B',
 				'gold'		=> 100,
 				'cost'		=> 3.99,
-				'currency'	=> 'usd',
+				'currency'	=> 'usd', //type USD to show in dollar and EUR to make in euro
 				'image'		=> 'package_b.jpg'
 			),
 			array ( 
 				'name'		=> 'Package C',
 				'gold'		=> 250,
 				'cost'		=> 7.99,
-				'currency'	=> 'usd',
+				'currency'	=> 'usd', //type USD to show in dollar and EUR to make in euro
 				'image'		=> 'package_c.jpg'
 			),
 			array ( 
 				'name'		=> 'Package D',
 				'gold'		=> 600,
 				'cost'		=> 15.99,
-				'currency'	=> 'usd',
+				'currency'	=> 'usd', //type USD to show in dollar and EUR to make in euro
 				'image'		=> 'package_d.jpg'
 			),
 		),
@@ -65,7 +65,7 @@ $AppConfig = array (
 				'key'			=> '',
 				'testKey'		=> '',
 				'returnKey'		=> '',
-				'currency'		=> 'usd'
+				'currency'		=> 'usd' //type USD to make in dollar and EUR to make in euro
 			),
 			'onecard'	=> array (
 				'testMode'		=> FALSE,
@@ -76,14 +76,14 @@ $AppConfig = array (
 				'key'			=> '',
 				'testKey'		=> '',
 				'returnKey'		=> '',
-				'currency'		=> 'usd'
+				'currency'		=> 'usd' //type USD to make in dollar and EUR to make in euro
 			),
 			'paypal'	=> array (
 				'testMode'		=> false,
 				'name'			=> 'PayPal',
 				'image'			=> 'paypal_solution_graphic-US.gif',
-				'merchant_id'	=> 'suport@extreemgaming.com',//rebhee62@gmail.com
-				'currency'		=> 'USD'
+				'merchant_id'	=> 'Sokifilou@gmail.com',
+				'currency'		=> 'EUR' //type USD to make in dollar and EUR to make in euro
 			)
 		)
 	)
